db22c0ac6bebd09c34079e9d0a5554d0b0c566df
fabrezi <fkschon@gmail.com>
2024-11-19 10:23:32 -0500

    fas

--- store/src/App.js
+++ store/src/App.js
@@ -9,20 +9,9 @@ function App() {
         <meta charSet="utf-8"/>
         <title>Show me the MONEY!!!</title>
       </Helmet>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>THE COURTIER</h1>
+      </div>
     </div>
   );
 }
