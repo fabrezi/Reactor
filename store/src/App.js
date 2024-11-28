d3d07dd8924afbdc5bfbc0a30314bbd82190c9b0
fabrezi <fkschon@gmail.com>
2024-11-28 07:39:32 -0500

    add

--- store/src/App.js
+++ store/src/App.js
@@ -4,7 +4,7 @@ import {Helmet} from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: `purple`}}>
       <Helmet>
         <meta charSet="utf-8"/>
         <title>Welcome to courtier.</title> 
@@ -13,7 +13,12 @@ function App() {
         <h1>THE COURTIER</h1>
       </div>
       {/*<img src={require('C:\Users\projects\Reactor\images\mogulinsuit.PNG')}/>*/}
-      <h2>Fuck OFF</h2>
+      <h2>YOu got Mail!!</h2>
+      <div>
+        <button>MENU</button>
+        <button>RESERVATION</button>
+        <button>CONTACT-US</button>
+      </div>
     </div>
   );
 }
